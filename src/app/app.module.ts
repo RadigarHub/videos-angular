9f02cf9ebc50057d242719f96736c7038ec0d5ff
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-11-09 16:13:04 +0100

    Class 295 and 296

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     routing
   ],
   providers: [
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { global } from './global';
+
+@Injectable()
+export class UserService {
+
+  public url: string;
+
+  constructor(
+    public _http: HttpClient,
+  ) {
+    this.url = global.url;
+  }
+
+  prueba() {
+    return 'Hola mundo desde un servicio de angular';
+  }
+
+}
