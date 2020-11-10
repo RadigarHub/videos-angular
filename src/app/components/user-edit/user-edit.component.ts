8b657d6064ee4907e68d06e4e6abb3469f4d9a86
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-11-10 13:52:06 +0100

    Class 305

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
 
   public page_title: string;
+  public user: User;
+  public status: string;
+  public identity;
+  public token: string;
 
-  constructor() {
-    this.page_title = 'Ajustes de usuario';
+  constructor(
+    private _userService: UserService
+  ) {
+    this.page_title = "Ajustes de usuario";
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.user = new User(this.identity.sub, this.identity.name, this.identity.surname, this.identity.email, '', 'ROLE_USER', '');
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form) {
+    
   }
 
 }
