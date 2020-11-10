a536b9264704d7a66bce8db9cde344ebb85deac7
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-11-10 12:52:27 +0100

    Class 300

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 
@@ -18,7 +19,9 @@ export class LoginComponent implements OnInit {
   public status: string;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ) {
     this.page_title = "Identifícate";
     this.user = new User(1, '', '', '', '', 'ROLE_USER', '');
@@ -40,8 +43,14 @@ export class LoginComponent implements OnInit {
             response => {
               if (response.status != 'error') {
                 this.token = response;
-                console.log(this.user);
-                console.log(this.token);
+                // console.log(this.user);
+                // console.log(this.token);
+                
+                localStorage.setItem('token', this.token);
+                localStorage.setItem('identity', JSON.stringify(this.identity));
+
+                this._router.navigate(['/inicio']);
+
               } else {
                 this.status = 'error';
               }
