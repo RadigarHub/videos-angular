ae8bc551291626c08c983e1d0840184ce7372e7d
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-11-11 11:16:02 +0100

    Class 311

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,32 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { VideoService } from '../../services/video.service';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [UserService]
+  providers: [UserService, VideoService]
 })
 export class HomeComponent implements OnInit {
 
   public page_title: string;
   public identity;
   public token;
+  public videos;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _videoService: VideoService
   ) {
     this.page_title = "Mis vídeos";
   }
 
   ngOnInit(): void {
     this.loadUser();
+    this.getVideos();
   }
 
   loadUser() {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
+  }
+
+  getVideos() {
+    this._videoService.getVideos(this.token).subscribe(
+      response => {
+        this.videos = response.videos;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
