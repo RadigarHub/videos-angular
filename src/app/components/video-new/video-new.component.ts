f338e4fc294f46c3836c9d0a37175108ed55a7d0
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-11-11 10:26:27 +0100

    Class 307

--- src/app/components/video-new/video-new.component.ts
+++ src/app/components/video-new/video-new.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { Video } from '../../models/video';
 
 @Component({
   selector: 'app-video-new',
   templateUrl: './video-new.component.html',
-  styleUrls: ['./video-new.component.css']
+  styleUrls: ['./video-new.component.css'],
+  providers: [UserService]
 })
 export class VideoNewComponent implements OnInit {
 
   public page_title: string;
+  public identity;
+  public token: string;
+  public video: Video;
 
-  constructor() {
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
+  ) {
     this.page_title = 'Guardar un nuevo vídeo favorito';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
-
+  
   ngOnInit(): void {
+    this.video = new Video(1, this.identity.sub, '', '', '', '', null, null);
   }
 
 }
